5bf72104a600f830904b695aa7d37904fb4390ee
Samuel Kelemen <Samuel@Kelemen.us>
2018-08-10 17:05:17 -0400

    going to thailand

---
+++ ExampleService/ExampleService/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExampleService.Models
+{
+    public class Item
+    {
+        public ulong ItemId { get; }
+        public string ItemName { get; }
+        public int ItemValue { get; }
+        public DateTime Created { get; }
+
+        public Item(ulong itemId, string itemName, DateTime created, int value)
+        {
+            this.ItemId = itemId;
+            this.ItemName = itemName;
+            this.Created = created;
+            this.ItemValue = value;
+        }
+
+        public Item()
+        {
+            ItemId = 55;
+            ItemName = "item";
+            ItemValue = 1;
+            Created = DateTime.Now;
+        }
+    }
+}
--- ExampleService/ExampleService/Program.cs
+++ ExampleService/ExampleService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ExampleService
 {
@@ -6,7 +7,63 @@ namespace ExampleService
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Models.Item item = new Models.Item();
+            ItemProcessor processor = new ItemProcessor();
+            Models.Item item2 = processor.Process(item);
+            Console.WriteLine($"OG: {item.ItemValue}");
+            Console.WriteLine($"NG: {item2.ItemValue}");
+            ItemIncrementer incrementer = new ItemIncrementer();
+            //incrementer.OnReceived += new ItemIncrementer.dgRaiseEvent
+            IReadOnlyCollection<string> readonlyCollection = new List<string>();
+            
+            Console.ReadKey();
         }
     }
+
+    class ItemIncrementer
+    {
+        public delegate void dgRaiseEvent();
+        public event dgRaiseEvent OnReceived;
+
+        public Models.Item Increment(Models.Item item)
+        {
+            return new Models.Item(item.ItemId, item.ItemName, item.Created, item.ItemValue + 1);
+        }
+    }
+
+    class ItemProcessor : ProcessorBase<Models.Item>
+    {
+        public new Models.Item Process(Models.Item item)
+        {
+            return new Models.Item(item.ItemId, item.ItemName, item.Created, item.ItemValue + 1);
+        }
+
+    }
+
+    public abstract class ProcessorBase<T> : IProcessor<T>
+    {
+        public virtual T Process(T item)
+        {
+            Console.WriteLine($"{item.GetType()} Item processed.");
+            return item;
+        }
+    }
+        
+
+    interface IProcessor<T>
+    {
+        T Process(T item);
+    }
+
+
+    interface INode
+    {
+        INode Next();
+    }
+
+
+
+
+
+    
 }
